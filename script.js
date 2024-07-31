779f1abfc6ad3c905e93fcfcd20739c9ace0cd1d
AbdallaMon <abdotlos60@gmail.com>
2024-07-31 12:12:47 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const logo = document.querySelector(".animate-logo");
+  const textElements = document.querySelectorAll(".animate-text");
+  const buttonElements = document.querySelectorAll(".button");
+
+  // Animate the logo first
+  setTimeout(() => {
+    logo.classList.add("animate");
+  }, 500);
+
+  // Animate text elements
+  textElements.forEach((el, index) => {
+    setTimeout(() => {
+      el.classList.add("animate");
+    }, (index + 1) * 500);
+  });
+  console.log(textElements.length);
+  // Animate button elements after the last text element is animated
+  buttonElements.forEach((el, index) => {
+    setTimeout(() => {
+      el.classList.add("animate-button");
+    }, 2000);
+  });
+});
